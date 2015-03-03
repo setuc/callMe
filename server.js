7f505044c97c7224a5aa3cd2010352c3397da5bd
setuc <setu.chokshi+github@gmail.com>
2015-03-03 11:59:13 +0800

    Added server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+/**
+ * Created by setuc on 3/3/2015.
+ */
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
